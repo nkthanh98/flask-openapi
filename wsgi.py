9889e363161aa975b8263dde1ebd0d2ea42cf7a3
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2020-05-29 14:35:26 +0700

    add pre-commit

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 
 import os
-import logging.config
 from app import (
     models,
     apis,
@@ -9,7 +8,6 @@ from app import (
 )
 
 
-logging.config.fileConfig('logging.ini')
 ENV = os.getenv('ENV', 'production')
 application = apis.create_wsgi(ENV)
 models.load_config(ENV)
